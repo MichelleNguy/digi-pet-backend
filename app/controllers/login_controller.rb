4aaee8190ca74886d031ec854a84d837faf8b6dc
MichelleNguy <plasticwatering@gmail.com>
2019-12-07 02:18:17 -0500

    moved responsibility to add money to users account with each unique login to user model

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,12 +4,7 @@ class LoginController < ApplicationController
   def create
     user = User.find_by("lower(username) = ?", params[:username].downcase)
     if user && user.authenticate(params[:password])
-      time = Time.new
-      if user.last_day != time.day
-        user.bank += 100
-        user.last_day = time.day
-        user.save
-      end
+      user.daily_bonus
       render json: authentication_json(user.id)
     else
       render json: { errors: [ "No" ] }, status: :unprocessable_entity
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ApplicationRecord
 
     validates_presence_of :username
     validates_uniqueness_of :username, :case_senstive => false
+
+    # checks current day and user's last login day, if they are not
+    # equal, a user will receive 100 dollars per unique login
+    def daily_bonus
+        time = Time.new
+        if self.last_day != time.day
+            self.bank += 100
+            self.last_day = time.day
+            self.save
+        end
+    end
+
+
 end

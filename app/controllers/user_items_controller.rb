360e016a058cb5c741e8679f2126e34d621b26ff
MichelleNguy <plasticwatering@gmail.com>
2019-12-08 18:39:51 -0500

    code clean up for user_item controller

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -3,31 +3,31 @@ class UserItemsController < ApplicationController
 
   # POST /user_items
   def create
-    @user_item = UserItem.new(user_item_params)
-    @user = User.find(params[:user_id])
-    @item = Item.find(params[:item_id])
+    user_item = UserItem.new(user_item_params)
+    user = User.find(params[:user_id])
+    item = Item.find(params[:item_id])
     
-    if @user.bank <= @item.price
+    if user.bank <= item.price
       render json: {poor: true}, status: :unprocessable_entity
-    elsif @user_item.save
-      @user.bank -= @item.price
-      @user.save
-      render json: @user_item, status: :created, location: @user_item
+    elsif user_item.save
+      user.bank -= item.price
+      user.save
+      render json: user_item, status: :created, location: (user_item)
     else
-      render json: @user_item.errors, status: :unprocessable_entity
+      render json: user_item.errors, status: :unprocessable_entity
     end
 
   end
 
   # DELETE /user_items/1
   def destroy
-    @user_item.destroy
+    user_item.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user_item
-      @user_item = UserItem.find(params[:id])
+      user_item = UserItem.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.

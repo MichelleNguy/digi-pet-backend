5e6fc96c7442adde950ee24e5178f4d263b7cd59
MichelleNguy <plasticwatering@gmail.com>
2019-12-09 01:49:32 -0500

    added comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,15 +15,19 @@ class ApplicationController < ActionController::API
   # In order to authorize
   def logged_in_user_id
     token = request.headers["Authorization"]
+    # something which might raise an exception
     begin 
       decoded_payload = JWT.decode(token, hmac_secret, true, { algorithm: 'HS256' })
       return decoded_payload.first["user_id"].to_i
+    # code that deals with some exception
     rescue
       return nil
     end
   end
 
+
   def logged_in?
+    # double bang converts value to boolean
     !!logged_in_user_id
   end
 

5c7ed164e488a21c14743b43ae21e438b4631a6b
MichelleNguy <plasticwatering@gmail.com>
2019-12-08 19:13:45 -0500

    code clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::API
 
   # In order to log in
   def make_token(user_id)
-    puts "something"
     payload = { user_id: user_id }
     JWT.encode(payload, hmac_secret, 'HS256')
   end
